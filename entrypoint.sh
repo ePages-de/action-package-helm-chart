ba8b49f48232e086846c59fb1c1b775ce9e8358b
Roberto Filho <r.welzel@epages.com>
2022-01-11 15:44:57 +0100

    EPNG-8708: Using different ref.

--- entrypoint.sh
+++ entrypoint.sh
@@ -72,4 +72,4 @@ curl -sS --fail -u "${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD}" \
   -T "${helm_package_name}"
 
 # Set the output parameters
-echo "::set-output name=chart-version::$new_chart_version"
+echo "::set-output name=chart-version::${new_chart_version}"
